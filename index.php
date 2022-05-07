23f71e191f07ad3baa7d1be1c99a91d05a7129d9
Orlina M Brabar <90068655+OrLina1125@users.noreply.github.com>
2022-05-07 10:55:10 +0900

    add for loop19

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Loop 17</title>
+</head>
+<body>
+    <?php
+    //Loop
+    //While loop
+    //DO while loop
+    //For loop (ini)
+    //Foreach loop
+    
+    for ($x = 1 $x <= 10; $x++){
+        echo "Hi there<br>";
+        $x++;
+
+    }
+    ?>
+</body>
+</html>
